e05c8bdcdfe442c83b104ef139320de3269c63d2
SpringDong <springu3d@yeah.net>
2019-06-02 10:48:58 +0800

    add : displaylist
    Co-Authored-By: springdong <springu3d@yeah.net>

---
+++ HelloOpenGL/DisplayList.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <windows.h>
+#include <gl/GL.h>
+#include <functional>
+
+class DisplayList
+{
+public:
+	GLuint mDisplayList;
+	void Init(std::function<void()>foo);
+	void Draw();
+};
+
